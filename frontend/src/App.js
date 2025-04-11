eb8d9d3fe264c1c16f48346296d2980433af5fad
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2025-04-11 02:14:24 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <h1>Campus Club Event Management System</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
